3e081e22310b7c2a5fbe4e6755cf363417db320d
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-27 15:50:49 +0100

    fix splicein destrory

--- destroy.php
+++ destroy.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $id = $_POST['id'] ?? '';
 // var_dump($id);
@@ -7,20 +7,20 @@ $response = [
     'success' => true,
 ];
 
- // leggere il contenuto del file json 
- $todos_string = file_get_contents('./todos.json');
- //decodicficare il file json per ottenere l array
- $todos = json_decode($todos_string, true);
+// leggere il contenuto del file json 
+$todos_string = file_get_contents('./todos.json');
+//decodicficare il file json per ottenere l array
+$todos = json_decode($todos_string, true);
 
 //  codice per rimuove la todo
-    $todoDel = array_slice($todos,intval($id),1);
+array_splice($todos, intval($id), 1,);
 
-    $response['result'] = $todos;
-   // risalvare il file:
-  // - codificare la stringa json dall'array di todos
-  $todos_string = json_encode($todos);
-  // - salvare il file con il nuovo contenuto
-  file_put_contents('./todos.json', $todos_string);
+$response['result'] = $todos;
+// risalvare il file:
+// - codificare la stringa json dall'array di todos
+$todos_string = json_encode($todos);
+// - salvare il file con il nuovo contenuto
+file_put_contents('./todos.json', $todos_string);
 
 header('Content-Type: application/json');
 echo json_encode($response);
--- js/main.js
+++ js/main.js
@@ -4,61 +4,66 @@ createApp({
   data() {
     return {
       title: 'To Do List!',
-      newTodo:'',
-      todos:[],
+      newTodo: '',
+      todos: [],
     }
   },
   methods: {
-    fetchData(){
-        // chiamata al server che mi da la risposta e popolo l array todos 
-        axios.get('server.php').then((res)=>{
-            console.log(res.data.result);
-            this.todos = res.data.result;
-        })
+    fetchData() {
+      // chiamata al server che mi da la risposta e popolo l array todos 
+      axios.get('server.php').then((res) => {
+        console.log(res.data.result);
+        this.todos = res.data.result;
+      })
     },
-    storeTodo(){
-        // console.log('add',this.newTodo);
-        if(this.newTodo.trim() !== ''){
-          const data = {
-              todo:{
-                  'text': this.newTodo,
-              }
+    storeTodo() {
+      // console.log('add',this.newTodo);
+      // chiamata di tipo post che contatta store per inserire un altra todo 
+      if (this.newTodo.trim() !== '') {
+        const data = {
+          todo: {
+            'text': this.newTodo,
           }
-          // console.log(data.todo)
-          axios.post('store.php', data,{
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          }).then((res)=>{
-            const respData = res.data;
-            if (respData.success === true) {
-              this.todos = res.data.result;
-            }
-            this.newTodo = '';
-          })
         }
-    },
-    deleteTodo(index){
-      console.log('delete',index);
-      const data = {
-        "id" : index,
+        // console.log(data.todo)
+        axios.post('store.php', data, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }).then((res) => {
+          const respData = res.data;
+          if (respData.success === true) {
+            this.todos = res.data.result;
+          }
+          this.newTodo = '';
+        })
       }
-     axios.post('destroy.php',data,{
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-     }).then((res)=>{
-      console.log(res);
-     })
     },
-    toggleDone(t){
-      console.log('toggle',t.done);
-      if(t.done === false){
+    deleteTodo(index) {
+      console.log('delete', index);
+      const data = {
+        "id": index,
+      }
+      axios.post('destroy.php', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }).then((res) => {
+        console.log(res);
+        const respData = res.data;
+        if (respData.success === true) {
+          this.todos = res.data.result;
+        }
+      })
+    },
+    toggleDone(t) {
+      console.log('toggle', t.done);
+      if (t.done === false) {
         return t.done = true
-      } else{
+      } else {
         return t.done = false
       }
-      
+
     }
 
   },
